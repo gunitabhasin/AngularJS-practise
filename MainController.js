0d89fd036eb788d907fc59578958ba5ca8ba4cd6
gunitabhasin <gunita.bhasin@gmail.com>
2016-01-15 17:49:40 +0000

    Update MainController.js

--- MainController.js
+++ MainController.js
@@ -2,13 +2,13 @@ app.controller('MainController', ['$scope', function($scope) {
   $scope.apps =
   [
     {
-      icon: 'img/move.jpg',
+      icon: 'https://sheholdsthepromise.files.wordpress.com/2015/02/o-jumping-jacks-facebook.jpg',
       title: 'MOVE',
       developer: 'MOVE, Inc.',
       price: 0.99
     },
     {
-      icon: 'img/gameboard.jpg',
+      icon: 'https://upload.wikimedia.org/wikipedia/commons/d/d7/Uckers_gameboard_graphic.png',
       title: 'Gameboard',
       developer: 'Armando P.',
       price: 1.99
@@ -20,7 +20,7 @@ app.controller('MainController', ['$scope', function($scope) {
       price: 1.99
     },
     {
-      icon: 'img/shutterbugg.jpg',
+      icon: 'http://pngimg.com/upload/bicycle_PNG5378.png',
       title: 'Shutterbugg',
       developer: 'Chico Dusty',
       price: 2.99
